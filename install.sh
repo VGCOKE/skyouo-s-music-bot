e74ea4261fda9fd7da74ef11ecf33079b12f05c7
ShaoJie Lien <56823143+NCT-skyouo@users.noreply.github.com>
2020-11-09 20:11:18 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# 創建一個 新專案後, 創建 .replit 檔案, 打: run = "./install.sh"
+echo "REPL.IT 專用安裝檔"
+echo "run = \"git clone https://github.com/NCT-skyouo/skyouo-s-music-bot.git && mv skyouo-s-music-bot/* ./ && npm i && rm -rf skyouo-s-music-bot && echo '完成'\"" >> .replit
